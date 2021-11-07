7a90215e6a2577485850104b989e533bf8d137be
TontheTop <tuanak691@gmail.com>
2021-11-07 12:01:03 +0700

    add AutoKey for Ticket table

--- model/model_ticket.js
+++ model/model_ticket.js
@@ -1,34 +1,36 @@
 module.exports = (sequelize, Datatypes) => {
-  const Project = sequelize.define("ticket", {
-    price:{
-      type:Datatypes.INTEGER,
-    },
-    time_booking:{
-      type:Datatypes.DATE,
-      defaultValue: sequelize.literal('NOW()'),
+    const Project = sequelize.define("ticket", {
+            id: {
+                type: Datatypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            price: {
+                type: Datatypes.INTEGER,
+            },
+            time_booking: {
+                type: Datatypes.DATE,
+                defaultValue: sequelize.literal('NOW()'),
 
-    },
+            },
 
-    account_id:{
-      type:Datatypes.INTEGER,
-      primaryKey: true
-    },
-    show_time_id:{
-      type:Datatypes.INTEGER,
-      primaryKey: true
-    },
-    row:{
-      type:Datatypes.INTEGER,
-    },
-    column:{
-      type:Datatypes.INTEGER,
-    }
-    
-  },
-  {
-    timestamps: false
-  },
-  
-  {underscored: true});
-  return Project;
+            account_id: {
+                type: Datatypes.INTEGER,
+            },
+            show_time_id: {
+                type: Datatypes.INTEGER,
+            },
+            row: {
+                type: Datatypes.INTEGER,
+            },
+            column: {
+                type: Datatypes.INTEGER,
+            }
+
+        }, {
+            timestamps: false
+        },
+
+        { underscored: true });
+    return Project;
 };

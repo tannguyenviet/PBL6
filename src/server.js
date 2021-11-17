4d3a457ce95f97c7d6ae3f5d3d9c809d73abde84
Minh Tran <bluewings2605@gmail.com>
2021-11-17 16:01:10 +0700

    update server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const morgan = require('morgan');
+const morgan = require("morgan");
 require("dotenv").config();
 require("./auth/passport");
 const route = require("./routes/index");
@@ -8,6 +8,7 @@ const middlewares = require("./app/middlewares/notFound_errorHandleMiddleware");
 const passport = require("passport");
 const app = express();
 const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 const db = require("./utils/db");
 //
 db.sequelize.sync();
@@ -15,7 +16,7 @@ db.sequelize.sync();
 app.use(passport.initialize());
 //
 var corsOptions = {
-    origin: "http://localhost:8081"
+  origin: "http://localhost:3000",
 };
 app.use(cors(corsOptions));
 // parse requests of content-type - application/json
@@ -23,13 +24,13 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 //
-app.use(morgan('combined'));
-// 
+app.use(morgan("combined"));
+//
 route(app);
 //
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 // set port, listen for requests
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}.`);
 });

75f2b2671e3fc3ff34ccdf3568438d0bae2d4c81
Minh Tran <bluewings2605@gmail.com>
2021-12-02 20:01:29 +0700

    update server

--- src/routes/showtime.route.js
+++ src/routes/showtime.route.js
@@ -2,15 +2,20 @@ const showtime = require("../app/controllers/showtime");
 const passport = require("passport");
 const router = require("express").Router();
 
-
 // Create a new showtime - Manager
 router.post("/create", showtime.create);
 
 // Retrieve all showtime of a Film having a specific idTheater and a date - Manager
 router.get("/search", showtime.findAllWithIdFilmAndIdTheaterAndDate);
 
+// Retrieve a Showtime by id
+router.get("/:id", showtime.findById);
+
 // Retrieve all showtime of a Film having a specific idTheater and a date - Admin
-router.get("/searchForAdmin", showtime.findAllWithIdFilmAndIdTheaterAndDateForAdmin);
+router.get(
+  "/searchForAdmin",
+  showtime.findAllWithIdFilmAndIdTheaterAndDateForAdmin
+);
 
 // Update a showtime with id - Manager
 router.put("/:id", showtime.update);
@@ -18,6 +23,4 @@ router.put("/:id", showtime.update);
 // Delete a showtime with id - Manager
 router.delete("/:id", showtime.delete);
 
-
-
-module.exports = router
+module.exports = router;

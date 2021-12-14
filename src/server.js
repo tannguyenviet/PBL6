6ff155e43d83b417178f5daaa377c610653a853b
TontheTop <tuanak691@gmail.com>
2021-12-14 20:54:46 +0700

    ss

--- src/server.js
+++ src/server.js
@@ -37,7 +37,7 @@ const options = {
             description: "A simple Express Library API",
         },
         servers: [{
-            url: "http://localhost:8080",
+            url: `http://localhost:${PORT}`,
         }, ],
     },
     apis: ["./src/routes/*.js"],

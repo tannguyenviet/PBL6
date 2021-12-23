8ae4a7c313de0f3e27d9d379332247bf468c5179
tanschool <nguyenviettantan@gmail.com>
2021-12-23 16:52:09 +0700

    fix route payment

--- ApiService/src/routes/index.js
+++ ApiService/src/routes/index.js
@@ -1,31 +1,30 @@
-const tutorialRoute = require('./tutorial.route')
-const accountRoute = require('./account.route')
-const filmRoute = require('./film.route')
-const siteRouter = require('./site.route')
-const showtime = require('./showtime.route')
-const ticket = require('./ticket.route')
-const theater = require('./theater.route')
-const membership = require('./membership.route')
-const pricetype = require('./pricetype.route')
-const roomfilm = require('./roomfilm.route')
-const order = require('./order.route')
+const tutorialRoute = require("./tutorial.route");
+const accountRoute = require("./account.route");
+const filmRoute = require("./film.route");
+const siteRouter = require("./site.route");
+const showtime = require("./showtime.route");
+const ticket = require("./ticket.route");
+const theater = require("./theater.route");
+const membership = require("./membership.route");
+const pricetype = require("./pricetype.route");
+const roomfilm = require("./roomfilm.route");
+const order = require("./order.route");
 
 function route(app) {
+  // simple route
+  app.use("/payment", order);
+  app.use("/theater", theater);
+  app.use("/tutorials", tutorialRoute);
+  app.use("/film", filmRoute);
+  app.use("/account", accountRoute);
+  app.use("/showtime", showtime);
+  app.use("/ticket", ticket);
+  app.use("/membership", membership);
+  app.use("/pricetype", pricetype);
+  app.use("/roomfilm", roomfilm);
 
-    // simple route
-    app.use('/order', order);
-    app.use('/theater', theater);
-    app.use('/tutorials', tutorialRoute);
-    app.use('/film', filmRoute);
-    app.use('/account', accountRoute);
-    app.use('/showtime', showtime);
-    app.use('/ticket', ticket);
-    app.use('/membership', membership);
-    app.use('/pricetype', pricetype);
-    app.use('/roomfilm', roomfilm);
-
-    //get page Home
-    app.use('/', siteRouter)
+  //get page Home
+  app.use("/", siteRouter);
 }
 
-module.exports = route
+module.exports = route;

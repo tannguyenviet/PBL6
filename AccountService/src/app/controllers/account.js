d992f5d55f0c8edfcaeaf93b49823c190b271b20
tanschool <nguyenviettantan@gmail.com>
2021-12-24 08:39:46 +0700

    fix gmail

--- AccountService/src/app/controllers/account.js
+++ AccountService/src/app/controllers/account.js
@@ -332,7 +332,9 @@ exports.resetPassword = async (req, res) => {
     .save()
     .then(() => {
       const transporter = nodemailer.createTransport({
-        service: "gmail",
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
         auth: {
           user: process.env.EMAIL_ADDRESS, // ethereal us er
           pass: process.env.EMAIL_PASSWORD, // ethereal password
